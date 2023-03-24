3478fea18b44fb80c68e9ad1b8ca62ac974cd351
Taha Abbasi <taha.abbasi@ferrum.network>
2023-03-24 13:52:09 -0600

    Updated configuration to be dynamic

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+// src/config.ts
+import { NetworkConfigurations } from "./getSupplyAcrossNetworks";
+
+const networks: NetworkConfigurations = {
+  Ethereum: {
+    jsonRpcUrl: "https://nd-770-685-838.p2pify.com/e30d3ea257d1588823179ce4d5811a61",
+    tokenContractAddress: "0xe5caef4af8780e59df925470b050fb23c43ca68c",
+  },
+  BSC: {
+    jsonRpcUrl: "https://nd-605-906-592.p2pify.com/df9065025f5e18317e708040b1f2ab13",
+    tokenContractAddress: "0xa719b8ab7ea7af0ddb4358719a34631bb79d15dc",
+  },
+  Polygon: {
+    jsonRpcUrl: "https://nd-662-671-431.p2pify.com/72aea5a70bbd5482f9a498540072b1e1",
+    tokenContractAddress: "0xd99bafe5031cc8b345cb2e8c80135991f12d7130",
+  },
+  Arbitrum: {
+    jsonRpcUrl: "https://nd-674-145-610.p2pify.com/bc9acaa6f1386224186fb1e794c40c14",
+    tokenContractAddress: "0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda",
+  },
+  Avalanche: {
+    jsonRpcUrl: "https://nd-900-134-973.p2pify.com/0a4e07e77ebc245f0bf7839745b4803b/ext/bc/C/rpc",
+    tokenContractAddress: "0xe5caef4af8780e59df925470b050fb23c43ca68c",
+  },
+};
+
+export { networks };
