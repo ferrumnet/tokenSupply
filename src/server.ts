04a69300ecf06eab5d114068d67e7db1a41c7c95
Taha Abbasi <taha.abbasi@ferrum.network>
2023-03-30 19:41:28 -0600

    Added caching

--- src/server.ts
+++ src/server.ts
@@ -11,29 +11,8 @@ import cacheMiddleware from './cacheMiddleware';
 
 const app = express();
 const port = process.env.PORT || 8080;
-// const cache = new NodeCache({ stdTTL: 60 }); // 60 seconds TTL
 
 app.use(bodyParser.json());
-
-// const cacheMiddleware = (duration: number) => {
-//   return (req: Request, res: Response, next: NextFunction) => {
-//     const key = req.originalUrl;
-//     const cachedResponse = cache.get(key);
-
-//     if (cachedResponse) {
-//       res.send(cachedResponse);
-//     } else {
-//       const originalSend = res.send.bind(res);
-
-//       res.send = ((body: any) => {
-//         cache.set(key, body, duration);
-//         originalSend(body);
-//       }) as Response['send'];
-
-//       next();
-//     }
-//   };
-// }
 
 app.get("/totalSupplyAcrossNetworks", cacheMiddleware(60), async (req, res) => {
   try {
